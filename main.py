bc01a3b50f05b0ff93229f223bfe92a57b786dd3
Iago <iago.pontes@outlook.com>
2024-01-29 21:58:23 -0300

    1.0 - Pronto
    Informe a data no formato solicitado e a consulta criará uma playlist com as músicas pesquisadas.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+import spotify as sp
 from bs4 import BeautifulSoup
 
-# date = input("Informe a data da lista que deseja, no seguinte formato YYYY-MM-DD: ")
+date = input("Informe a data da lista que deseja, no seguinte formato YYYY-MM-DD: ")
 
-response = requests.get(f"https://www.billboard.com/charts/hot-100/2024-01-06")
+# 1 - Scraping the Billboard Hot 100
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}")
 billboard_page = response.text
 
 soup = BeautifulSoup(billboard_page, "html.parser")
@@ -15,4 +17,19 @@ for song in song_titles:
     song_name = " ".join(song.text.split())
     song_list.append(song_name)
 
-print(song_list)
+print(f"Lista de músicas: {song_list}")
+
+# 3 - Search Spotify for the Songs from Step 1
+music_uri_list = []
+print("Pesquisando músicas!")
+for song in song_list:
+    song_name = sp.search_music(song)
+    music_uri_list.append(song_name)
+
+# 4 - Creating Spotify Playlist
+print("Criando playlist.")
+playlist_id = sp.create_playlist(f"Playlist Billboard {date}")
+
+# 5 - Adding to Spotify Playlist
+print("Adicionado músicas a playlist.")
+sp.add_music_playlist(playlist_id, music_uri_list)
--- spotify.py
+++ spotify.py
@@ -2,32 +2,32 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+# 2 - Authentication with Spotify
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://example.com", scope="playlist-modify-private")
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+# id usuário
 current_user = sp.current_user()
 user_id = current_user['id']
-# print(user_id)
 
-# id musica spotify
-# music_search = sp.search(q="Rockin' Around The Christmas Tree", limit=1, offset=0, type="track", market=None)
-# print(music_search["tracks"]["items"][0]["album"]["uri"])
 
-# Criar playlist
-#new_playlist = sp.user_playlist_create(user=user_id, name="Playlist de Natal", public=False, collaborative=False, description="Playlist Billboard Natal")
-#playlist_id = new_playlist["id"]
-#print(playlist_id)
+def search_music(music_name):
+    """Retorna o URI da música pesquisada"""
+    music_search = sp.search(q=music_name, limit=1, offset=0, type="track", market=None)
+    music_uri = music_search["tracks"]["items"][0]['uri']
+    return music_uri
 
-sp.current_user_follow_playlist('52exwQf8X1EJniVH5Poffo')
 
-playlists = sp.user_playlists(user_id)
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
+def create_playlist(playlist_name):
+    """"Criar playlist"""
+    new_playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False, collaborative=False, description="Playlist Billboard Natal")
+    playlist_id = new_playlist["id"]
+    return playlist_id
+
+
+def add_music_playlist(playlist_id, music_uri_list):
+    """Adiciona uma lista de URI/URL a playlist"""
+    sp.playlist_add_items(playlist_id, music_uri_list)
