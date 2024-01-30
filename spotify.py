659ab7730d43c24acf97115acdd37fde9cc4a5f3
iagopontesbm <86330103+iagopontesbm@users.noreply.github.com>
2024-01-30 07:52:42 -0300

    Update spotify.py
    1.1 - Adicionados os tipos de variaveis e retornos nas funções do spotify.py

--- spotify.py
+++ spotify.py
@@ -14,20 +14,20 @@ current_user = sp.current_user()
 user_id = current_user['id']
 
 
-def search_music(music_name):
+def search_music(music_name: str) -> str:
     """Retorna o URI da música pesquisada"""
     music_search = sp.search(q=music_name, limit=1, offset=0, type="track", market=None)
     music_uri = music_search["tracks"]["items"][0]['uri']
     return music_uri
 
 
-def create_playlist(playlist_name):
-    """"Criar playlist"""
+def create_playlist(playlist_name: str) -> str:
+    """"Criar playlist e retorna o id"""
     new_playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False, collaborative=False, description="Playlist Billboard Natal")
     playlist_id = new_playlist["id"]
     return playlist_id
 
 
-def add_music_playlist(playlist_id, music_uri_list):
+def add_music_playlist(playlist_id: str, music_uri_list: list) -> None:
     """Adiciona uma lista de URI/URL a playlist"""
     sp.playlist_add_items(playlist_id, music_uri_list)
